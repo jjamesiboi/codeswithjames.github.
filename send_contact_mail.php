ccd2aa8f9dcc3b071423c57789e8e82d3a7f5e5f
James Messer <71551059+CodesWithJames@users.noreply.github.com>
2020-10-16 21:04:49 +0200

    Update send_contact_mail.php

--- send_contact_mail.php
+++ send_contact_mail.php
@@ -5,7 +5,7 @@ if(!empty($_POST["send"])) {
 	$subject = $_POST["subject"];
 	$content = $_POST["content"];
 
-	$toEmail = "admin@phppot_samples.com";
+	$toEmail = "jamesmesserking@gmail.com";
 	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
 	if(mail($toEmail, $subject, $content, $mailHeaders)) {
 	    $message = "Your contact information is received successfully.";
